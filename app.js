17c307db1fe4b630a5b2b33032a510d4f149510d
Timothy Miamba <timal0361@gmail.com>
2023-06-16 11:07:25 +0200

    Add UI and change game format to use buttons instead of console

--- app.js
+++ app.js
@@ -16,45 +16,62 @@ function getComputerChoice(){
     return choice;
 }
 
-function playRound(playerSelection, computerSelection){
-    //rock beats scissors
-    //paper beats rock
-    //scissors beats paper
-    playerSelection = playerSelection.toLowerCase();
+function incrementScore(scoreElement){
+    let score = Number(scoreElement.textContent);
+    score++;
+    scoreElement.textContent = score;
+}
+
+function playRound(e, result, userScore, computerScore){
+    const userSelection = this.classList.value; //gets the player choice
+    const computerSelection = getComputerChoice();
     let winnerDeclaration;
 
-    if (playerSelection === 'rock' && computerSelection === 'scissors'){
-        winnerDeclaration = 'You win! Rock beats Scissors';
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        winnerDeclaration = 'You lose! Rock beats Scissors';
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        winnerDeclaration = 'You win! Paper beats Rock';
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        winnerDeclaration = 'You lose! Paper beats Rock';
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        winnerDeclaration = 'You win! Scissors beats Paper';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        winnerDeclaration = 'You lose! Scissors beats Paper';
+    if (userSelection === 'rock' && computerSelection === 'scissors'){
+        winnerDeclaration = 'You win this round! Rock beats Scissors';
+        incrementScore(userScore);
+    } else if (userSelection === 'scissors' && computerSelection === 'rock') {
+        winnerDeclaration = 'You lose this round! Rock beats Scissors';
+        incrementScore(computerScore);
+    } else if (userSelection === 'paper' && computerSelection === 'rock') {
+        winnerDeclaration = 'You win this round! Paper beats Rock';
+        incrementScore(userScore);
+    } else if (userSelection === 'rock' && computerSelection === 'paper') {
+        winnerDeclaration = 'You lose this round! Paper beats Rock';
+        incrementScore(computerScore);
+    } else if (userSelection === 'scissors' && computerSelection === 'paper') {
+        winnerDeclaration = 'You win this round! Scissors beats Paper';
+        incrementScore(userScore);
+    } else if (userSelection === 'paper' && computerSelection === 'scissors') {
+        winnerDeclaration = 'You lose this round! Scissors beats Paper';
+        incrementScore(computerScore);
     } else {
-        winnerDeclaration = `It's a tie! You both picked ${playerSelection}`;
+        winnerDeclaration = `It's a tie! You both picked ${userSelection}`;
     }
-    return winnerDeclaration;
+    result.textContent = winnerDeclaration;
+}
+
+function checkCompletion(userScoreElement, computerScoreElement, result){
+    userScore = Number(userScoreElement.textContent);
+    computerScore = Number(computerScoreElement.textContent);
+
+    if (userScore === 5){
+        result.textContent = 'The game is over. You beat the computer!';
+    } else if (computerScore === 5){
+        result.textContent = 'The game is over. You lost to the computer.';
+    }
 }
 
 function game() {
-    console.log('ROCK PAPER SCISSORS');
-    let computerSelection;
-    let playerSelection;
+    const userSelections = document.querySelectorAll('button'); // references all selection buttons on webpage.
+    const userScore = document.querySelector('.user .score'); // references the span element showing the user's score.
+    const computerScore = document.querySelector('.computer .score'); // references the span element showing the computer's score.
 
-    for (let i = 1; i <= 5; i++) {
-        computerSelection = getComputerChoice();
-        playerSelection = prompt('Enter your choice between rock, paper and scissors: ');
+    const result = document.querySelector('.declaration'); //used to declare the winner on the webpage
 
-        winnerDeclaration = playRound(playerSelection, computerSelection);
-        console.log(winnerDeclaration);
-
-        (i != 5) ? console.log('Next round loading...') : console.log('Game complete.')
-    }
+    userSelections.forEach(selection => {
+        selection.addEventListener('click',playRound(e,result,userScore,computerScore));
+    })
 }
 
 game();
