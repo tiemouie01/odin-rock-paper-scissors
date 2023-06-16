3caa0957700e3336681d75ebde1efc38eceffc96
Timothy Miamba <timal0361@gmail.com>
2023-06-16 14:00:19 +0200

    Add feature that stops the game when one player gets to 5 points

--- app.js
+++ app.js
@@ -33,40 +33,51 @@ function playRound(){
 
     let winnerDeclaration;
 
-    if (userSelection === 'rock' && computerSelection === 'scissors'){
-        winnerDeclaration = 'You win this round! Rock beats Scissors';
-        incrementScore(userScore);
-    } else if (userSelection === 'scissors' && computerSelection === 'rock') {
-        winnerDeclaration = 'You lose this round! Rock beats Scissors';
-        incrementScore(computerScore);
-    } else if (userSelection === 'paper' && computerSelection === 'rock') {
-        winnerDeclaration = 'You win this round! Paper beats Rock';
-        incrementScore(userScore);
-    } else if (userSelection === 'rock' && computerSelection === 'paper') {
-        winnerDeclaration = 'You lose this round! Paper beats Rock';
-        incrementScore(computerScore);
-    } else if (userSelection === 'scissors' && computerSelection === 'paper') {
-        winnerDeclaration = 'You win this round! Scissors beats Paper';
-        incrementScore(userScore);
-    } else if (userSelection === 'paper' && computerSelection === 'scissors') {
-        winnerDeclaration = 'You lose this round! Scissors beats Paper';
-        incrementScore(computerScore);
-    } else {
-        winnerDeclaration = `It's a tie! You both picked ${userSelection}`;
+    if (!checkCompletion(userScore,computerScore,result)){
+        if (userSelection === 'rock' && computerSelection === 'scissors'){
+            winnerDeclaration = 'You win this round! Rock beats Scissors';
+            incrementScore(userScore);
+        } else if (userSelection === 'scissors' && computerSelection === 'rock') {
+            winnerDeclaration = 'You lose this round! Rock beats Scissors';
+            incrementScore(computerScore);
+        } else if (userSelection === 'paper' && computerSelection === 'rock') {
+            winnerDeclaration = 'You win this round! Paper beats Rock';
+            incrementScore(userScore);
+        } else if (userSelection === 'rock' && computerSelection === 'paper') {
+            winnerDeclaration = 'You lose this round! Paper beats Rock';
+            incrementScore(computerScore);
+        } else if (userSelection === 'scissors' && computerSelection === 'paper') {
+            winnerDeclaration = 'You win this round! Scissors beats Paper';
+            incrementScore(userScore);
+        } else if (userSelection === 'paper' && computerSelection === 'scissors') {
+            winnerDeclaration = 'You lose this round! Scissors beats Paper';
+            incrementScore(computerScore);
+        } else {
+            winnerDeclaration = `It's a tie! You both picked ${userSelection}`;
+        }
+        result.textContent = winnerDeclaration;
     }
-    result.textContent = winnerDeclaration;
 }
 
-// function checkCompletion(userScoreElement, computerScoreElement, result){
-//     userScore = Number(userScoreElement.textContent);
-//     computerScore = Number(computerScoreElement.textContent);
+function checkCompletion(userScoreElement, computerScoreElement, result){
+    
+    /* This function checks the if either of the players have reached the 5 points.
+       If any of the players pass this mark, the function returns a value of true,
+       signalling that the game has been completed. When the function returns a value
+       of true the game can no longer be continued and a winner is announced.*/
 
-//     if (userScore === 5){
-//         result.textContent = 'The game is over. You beat the computer!';
-//     } else if (computerScore === 5){
-//         result.textContent = 'The game is over. You lost to the computer.';
-//     }
-// }
+    userScore = Number(userScoreElement.textContent);
+    computerScore = Number(computerScoreElement.textContent);
+
+    if (userScore >= 5){
+        result.textContent = 'The game is over. You beat the computer!';
+        return true;
+    } else if (computerScore >= 5){
+        result.textContent = 'The game is over. You lost to the computer.';
+        return true;
+    }
+    return false;
+}
 
 function game() {
     const userSelections = document.querySelectorAll('button'); // references all selection buttons on webpage.
